fca61eb71cebde9c08170136fe7742e3804af5e1
rafiehayat <rafiehayat5@gmail.com>
2025-07-29 23:42:53 +0530

    ...

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,17 @@ const Navbar = () => {
       <div className='flex items-center gap-4'>
         {
             token
-            ? <div></div>
+            ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                        <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>navigate('my-appointment')} className='hover:text-black cursor-pointer'>My Appointment</p>
+                        <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                    </div>
+                </div>
+            </div>
             :         <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
         }
       </div>
